51ba95cb4c037c43f4003c96eb1bec10443c0216
kimkmin06 <kimkmin06@gmail.com>
2023-11-13 20:20:29 +0900

    union, any, unknown type

--- 02.기본타입/index.ts
+++ 02.기본타입/index.ts
@@ -15,3 +15,20 @@ let project: { member: string[], days: number, started: boolean } = {
   days: 30,
   started: true,
 };
+
+let Member1: (number | string)[] = [1, '2', 3];
+let Obj1: { a: string | number } = { a: '123' };
+
+// Union Type: 타입 2개 이상 합친 타입
+let UnionTypeMember: number | string = 123;
+UnionTypeMember = 'kim';
+
+// Any Type: 모든 자료형 허용 => 타입실드 해제
+let AnyTypeMember: any;
+AnyTypeMember = 123;
+AnyTypeMember = [];
+
+// Unknown Type: 모든 자료형 허용 => any보다 안정성 높음, 엄격함(수학연산...->unknown은 number 아님)
+let UnknownTypeName: unknown;
+UnknownTypeName = 123;
+UnknownTypeName = {};
