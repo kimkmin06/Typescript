0575fb8a6a88af59e07612cc7761b5c1b3d799f1
kimkmin06 <kimkmin06@gmail.com>
2023-11-13 19:57:48 +0900

    ts 기본 타입

---
+++ 02.기본타입/index.ts
@@ -0,0 +1,17 @@
+let TypeName: string = 'kim';
+let TypeAge: number = 50;
+let MarryYn: boolean = true;
+let ArrMember: string[] = ['kim', 'park'];
+let UnionName: { member1: string, member2: string } = { member1: 'kim', member2: 'park' };
+
+const MyName: string = 'Kim KyeongMin';
+let MyAge: number = 18;
+const MyBirth: string = 'Daegu';
+
+let MyFavorite: { song: string, singer: string } = { song: "in the stars", singer: "benson boone" };
+
+let project: { member: string[], days: number, started: boolean } = {
+  member: ['kim', 'park'],
+  days: 30,
+  started: true,
+};
